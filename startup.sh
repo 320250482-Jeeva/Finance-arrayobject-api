cacf745e5e94486036fedfefeaa8d30206ae486c
Jeeva <jeeva.s@philips.com>
2025-11-14 17:37:57 +0530

    Delete startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-# Install dependencies (optional if already installed)
-pip install -r requirements.txt
-
-# Start FastAPI app with Gunicorn and Uvicorn workers
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker app:app
