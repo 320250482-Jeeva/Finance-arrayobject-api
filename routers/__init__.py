227859c5e74a3b6d7e19673e3057b98c4b360f7f
Jeeva <jeeva.s@philips.com>
2026-01-16 15:23:19 +0530

    Update __init__.py

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,4 +1,4 @@
 from .array_converter import router as array_router
-from .ppt_email import router as ppt_email_router
+from .ppt_email import app as ppt_email_router
 
 __all__ = ["array_router", "ppt_email_router"]
